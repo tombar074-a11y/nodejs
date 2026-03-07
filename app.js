b558e55fb9fdecfb5ba27a798edcc4729e250830
tombar074-a11y <tombar074@gmail.com>
2026-03-07 20:10:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
-
+const express = require("express");
 const app = express();
-const PORT = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Use the router for handling routes
-app.use('/', indexRouter);
+app.get("/", (req, res) => {
+  res.send("Leadflow webhook running");
+});
 
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+app.post("/whatsapp", (req, res) => {
+  const phone = req.body.From;
+  const message = req.body.Body;
 
+  console.log("New WhatsApp message:");
+  console.log(phone, message);
+
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log("Server running on port " + PORT);
 });
