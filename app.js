8a81f75a783135d4d1f1a5c302585d1d21a5aa67
tombar074-a11y <tombar074@gmail.com>
2026-03-09 17:25:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL, 
   ssl: {
     rejectUnauthorized: false
   }
